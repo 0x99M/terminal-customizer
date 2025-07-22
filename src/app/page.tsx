9c80ab7354dcc2e24565972e0a5c0cd700fb6ff5
Mustafa <ThreeMSEDT@users.noreply.github.com>
2025-07-22 07:20:58 +0300

    Implement FontSizeSelector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import FontSelector from "./components/font-selector";
+import FontSizeSelector from "./components/font-size-selector";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen grid grid-rows-[1fr_7fr_1fr] gap-4 p-4">
+    <div className="h-screen w-screen grid grid-rows-[1fr_7fr_1fr] gap-4 p-8">
       <div className="bg-gray-100 rounded-lg flex items-center justify-center">
         <h1 className="text-2xl font-semibold">Header Section</h1>
       </div>
 
       <div className="grid grid-cols-5 gap-4">
-        <div className="flex flex-col items-center justify-start">
+        <div className="flex flex-col items-center justify-start gap-8">
           <FontSelector />
+          <FontSizeSelector />
         </div>
-        <div className="col-span-3 flex flex-col items-center justify-center gap-4">
+        <div className="col-span-3 flex flex-col items-center justify-center">
           <h2 className="text-xl font-medium">Main Content</h2>
         </div>
         <div className="bg-blue-100 rounded-lg flex items-center justify-center">
